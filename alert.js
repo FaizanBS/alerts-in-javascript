098ab21c585c617ba768d76528c4f30cc095b922
Faizan <114131372+FaizanBS@users.noreply.github.com>
2023-05-28 08:12:38 +0500

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,23 @@
+
+//     alert in javascript in broser
+//     argumenting string returns nothing
+//   let a =  alert("This is a message apperad on site");
+// // let name=prompt("tell us your name?", "Guest");  //this prompt you to enter something
+
+// //  console.log(name);
+// let deletepost=confirm("You are going to delete this post sure?")
+// if (deletepost){
+//     console.log("your post is deleted")
+// }
+// else{
+//     console.log("Not deelted")
+// }
+let a = prompt("Enter your age");
+if (a>18){
+        console.log("You are entered")
+}
+else {
+    console.log("you are geeting out")
+}
+
+
